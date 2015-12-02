e32188b8f9dc7da330f42d77955f6a4bdf1c3489
RodrigoLGuimaraes <rolongui@yahoo.com.br>
2015-12-02 15:47:26 -0500

    line added - not working

--- sudoku_csp.py
+++ sudoku_csp.py
@@ -391,6 +391,60 @@ def picross_model(picross_constraints):
         con.add_satisfying_tuples(sat_tuples)
         cons.append(con)
 
+    #9 constraints for lines
+    line_list = picross_constraints[1]
+
+    for celli in range(len(picross_constraints[1])):
+        line = []
+        for cellj in range(len(picross_constraints[0])):
+            line.append(vars[celli*len(picross_constraints[0])+cellj])
+        con = Constraint("C(Line{})".format(str(celli)),line)
+
+        line_cons_input = line_list[celli]
+        list_initial_sat = list()
+
+        bucket_locations=list()
+        location = 0
+        element = 0
+        for number in line_cons_input:
+            bucket_locations.append(location)
+            location+=number
+            if(element != 0):
+                location+=1
+                list_initial_sat.append(False)
+            for i in range(number):
+                list_initial_sat.append(True)
+            element += 1
+        bucket_locations.append(location)
+
+        num_remaining_falses = len(line_list) - len(list_initial_sat)
+
+        num_boxes = len(line_cons_input) + 1
+
+        boxes = list()
+        for i in range(num_boxes):
+            boxes.append([])
+
+        sat_tuples = list()
+
+        for result in all_combinations(num_remaining_falses,boxes):
+            list_initial_sat_copy = list(list_initial_sat)
+            global_offset = 0
+            bucket_num = 0
+            for bucket in result:
+                local_offset = 0
+                for i in range(bucket[0]):
+                    list_initial_sat_copy.insert(global_offset+bucket_locations[bucket_num],False)
+                    local_offset+=1
+                global_offset+=local_offset
+                bucket_num+=1
+
+            #print(list_initial_sat_copy)
+            sat_tuples.append(tuple(list_initial_sat_copy))
+
+        con.add_satisfying_tuples(sat_tuples)
+        cons.append(con)
+
     picross_csp = CSP("Picross Solver Model", vars)
     for c in cons:
         picross_csp.add_constraint(c)
