ad4ad86d16f584a68b1f8c12f0f046a53c20c8d7
RodrigoLGuimaraes <rolongui@yahoo.com.br>
2015-12-05 19:57:56 -0500

    wild mario appears

--- Picross.py
+++ Picross.py
@@ -143,6 +143,7 @@ if __name__ == "__main__":
     #board = [cols,rows]
 
     #picross basic color model
+    '''
     rows1 = [[0],[1],[0],[0],[1]]
     cols1 = [[0],[0],[0],[0],[1],[0],[1]]
     board1 = [cols1,rows1]
@@ -151,6 +152,25 @@ if __name__ == "__main__":
     board2 = [cols2,rows2]
     boards = [board1,board2]
     colors = [[255,0,0],[0,0,255]]
+    '''
+
+    #   color mario
+    #brown
+    cols1 = [[3],[1,1],[3],[1,1],[0],[0],[2,1],[2],[1],[1],[0]]
+    rows1 = [[0],[0],[3,1],[1,1,1],[1,2,1],[2,4],[0]]
+    board1 = [cols1,rows1]
+    #yellow
+    cols2 = [[0],[2],[2],[1,2],[5],[5],[1,1],[2,1],[2,1],[2],[1]]
+    rows2 = [[0],[0],[2,1],[1,3,3],[1,3,3],[4],[7]]
+    board2 = [cols2,rows2]
+    #red
+    cols3 = [[0],[1],[2],[2],[2],[2],[2],[1],[1],[1],[0]]
+    rows3 = [[5],[9],[0],[0],[0],[0],[0]]
+    board3 = [cols3,rows3]
+
+    boards = [board1,board2,board3]
+
+    colors = [[128,0,0],[255,255,0],[255,0,0]]
 
     color_picross_basic_solver(boards,colors)
 
